60e6364f54959164a8e94255f5d2debdb3935774
Christian <christian@sanglech.com>
2022-06-07 20:56:05 +0900

    added leet code problem

--- src/Days/Day3.java
+++ src/Days/Day3.java
@@ -75,4 +75,26 @@ public class Day3 {
 
         return sb.toString();
     }
+
+    public static boolean isPalindrome(String s) {
+        final StringBuilder sb = new StringBuilder();
+        for (Character c : s.toLowerCase().toCharArray()) {
+            if (Character.isLetterOrDigit(c)){
+                sb.append(c);
+            }
+        }
+
+        int i =0;
+        int j = sb.length()-1;
+
+        while(i<j){
+            if(sb.charAt(i) != sb.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
 }
